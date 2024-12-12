da6032e89b276113ac24477a0cfbd82557ce6fdd
Shiv Chauhan <shivchauhan795@gmail.com>
2024-12-12 11:29:11 +0530

    bug fixing

--- src/app.ts
+++ src/app.ts
@@ -47,7 +47,9 @@ passport.use('jwt', jwtStrategy);
 if (config.env === 'production') {
   app.use('/v1/auth', authLimiter);
 }
-
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
 // v1 api routes
 app.use('/v1', routes);
 
